3b8072b431b8e296014eeb24860da97c5621bb2c
Mustafa Kabalak <85291323+tulpar34@users.noreply.github.com>
2022-10-14 21:17:14 +0300

    ödev 13 tamanlandı

--- cMLr3asK/index.js
+++ cMLr3asK/index.js
@@ -1,16 +1,27 @@
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
-let count = 0
+let saveEl = document.getElementById("save-el");
+let gecmisBilgi = localStorage.getItem("gecmisBilgi");
+const toplamEl = document.getElementById("totalpeople");
+if (gecmisBilgi !== null) {
+  saveEl.textContent = localStorage.getItem("gecmisBilgi");
+} else {
+  saveEl.textContent = "";
+}
+
+let countEl = document.getElementById("count-el");
+let count = 0;
+let toplamYolcu = 0;
 
 function increment() {
-    count += 1
-    countEl.textContent = count
+  count += 1;
+  countEl.textContent = count;
 }
 
 function save() {
-    let countStr = count + " - "
-    saveEl.textContent += countStr
-    localStorage.setItem( "gecmisBilgi", saveEl.textContent)
-    countEl.textContent = 0
-    count = 0
+  let countStr = count + " - ";
+  saveEl.textContent += countStr;
+  toplamYolcu += count;
+  localStorage.setItem("gecmisBilgi", saveEl.textContent);
+  toplamEl.textContent = toplamYolcu;
+  countEl.textContent = 0;
+  count = 0;
 }
